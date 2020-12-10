a01f465a85e137e83b822c99ce7186ba3ab7d71a
Mayank Mahajan <mayank.mongoosetech@gmail.com>
2020-12-10 19:43:03 +0530

    made succesfull routing with common navbar

--- test.js
+++ test.js
@@ -2,9 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.set ('view engine','ejs')
+
+const checkUrl=function(req, res, next){
+  console.log("current route is ",req.url)
+  next();
+}
+app.use(checkUrl);
+
+
+
 app.get('/', (req, res) => {
   res.render('index.ejs');
 })
+
+app.get('/contact', (req, res) => {
+  
+  res.render('contact.ejs');
+})
+
+app.get('/pricing', (req, res) => {
+
+  res.render('pricing.ejs');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
